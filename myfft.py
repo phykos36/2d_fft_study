6243d40206af46909a8dc32f730ae836eb591262
Phykos36 <furutaku12@gmail.com>
2019-07-15 15:21:22 +0900

    renamed arguments and function

--- myfft.py
+++ myfft.py
@@ -3,21 +3,21 @@ import cv2
 import matplotlib.pyplot as plt
 import math
 
-def fourier(src):
+def power_spectre(src):
     fimg = np.fft.fft2(src)
     mag = 20*np.log(np.abs(fimg))
     return mag
 
 def highpass_filter(src, a):
-    src = np.fft.fft2(src)
+    fsrc = np.fft.fft2(src)
     h, w = src.shape
     cy, cx = int(h/2), int(w/2)
     rh, rw = int(a*cy), int(a*cx)
-    src = 20*np.log(src)
-    fsrc = np.fft.fftshift(src)
-    fdst = fsrc.copy()
-    fdst[cy-rh:cy+rh, cx-rw:cx+rw] = 0
-    fdst = np.fft.fftshift(fdst)
+    fsrc = 20*np.log(fsrc)
+    shift_fsrc = np.fft.fftshift(fsrc)
+    shift_fdst = shift_fsrc.copy()
+    shift_fdst[cy-rh:cy+rh, cx-rw:cx+rw] = 0
+    fdst = np.fft.fftshift(shift_fdst)
     fdst = pow(math.e,fdst/20)
     dst = np.fft.ifft2(fdst)
     return np.uint8(dst.real)
@@ -25,11 +25,11 @@ def highpass_filter(src, a):
 def lowpass_filter(src, a):
     plt.subplot(131)
     plt.imshow(src, cmap = 'gray')
-    src = np.fft.fft2(src)
+    fsrc = np.fft.fft2(src)
     h, w = src.shape
     cy, cx = int(h/2), int(w/2)
     rh, rw = int(a*cy), int(a*cx)
-    fsrc = 20*np.log(src)
+    fsrc = 20*np.log(fsrc)
     plt.subplot(132)
     plt.imshow(np.uint8(fsrc.real), cmap = 'gray')
     fdst = fsrc
@@ -42,21 +42,21 @@ def lowpass_filter(src, a):
     return np.uint8(dst.real)    
 
 def main(path):
-    img = cv2.imread(path)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    mag = fourier(gray)
-    limg = lowpass_filter(gray,0.8)
-    himg = highpass_filter(gray,0.1)
+    src = cv2.imread(path)
+    gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    mag = power_spectre(gray)
+    low_img = lowpass_filter(gray,0.8)
+    high_img = highpass_filter(gray,0.1)
     plt.subplot(221)
     plt.imshow(gray, cmap = 'gray')
     plt.subplot(222)
     plt.imshow(mag, cmap = 'gray')
     plt.subplot(223)
-    plt.imshow(limg, cmap = 'gray')
+    plt.imshow(low_img, cmap = 'gray')
     plt.subplot(224)
-    plt.imshow(himg, cmap = 'gray')
+    plt.imshow(high_img, cmap = 'gray')
     plt.show()
-    cv2.imwrite("out.png", limg)
+    cv2.imwrite("out.png", low_img)
 
 
 # while True:
